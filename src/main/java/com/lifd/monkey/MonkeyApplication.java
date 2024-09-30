bb4bf52418a9cbc2ed309c2bb59a47e923f47f57
lifengdi <lifengdi@lifengdi.com>
2024-09-30 20:28:11 +0800

    first commit

---
+++ src/main/java/com/lifd/monkey/MonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.lifd.monkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyApplication.class, args);
+    }
+
+}
