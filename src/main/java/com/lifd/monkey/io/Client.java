663c39a07fe1b78a46a87821ec0bf4183778d11a
lifengdi <lifengdi@lifengdi.com>
2025-02-08 11:37:23 +0800

    nio 发送消息示例

--- src/main/java/com/lifd/monkey/io/Client.java
+++ src/main/java/com/lifd/monkey/io/Client.java
@@ -2,23 +2,44 @@ package com.lifd.monkey.io;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.channels.SocketChannel;
+import java.nio.*;
+import java.nio.channels.DatagramChannel;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
 /**
  * @author lifengdi
  * @createTime 2025/2/7 20:01
  */
 public class Client {
-    public static void main(String[] args) {
-        try {
-            SocketChannel socketChannel = SocketChannel.open();
-            socketChannel.configureBlocking(false);
-            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080));
-            while (!socketChannel.finishConnect()) {
-                System.out.println("正在连接");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void send() throws IOException {
+
+        //获取 DatagramChannel 数据报通道
+        DatagramChannel dChannel = DatagramChannel.open();
+
+        //设置为非阻塞
+        dChannel.configureBlocking(false);
+        ByteBuffer buffer =
+                ByteBuffer.allocate(1024);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("UDP 客户端启动成功！");
+        System.out.println("请输入发送内容:");
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            buffer.put((LocalDateTime.now() + " >>" + next).getBytes());
+            buffer.flip();
+
+            //通过 DatagramChannel 数据报通道发送数据
+            dChannel.send(buffer,
+                    new InetSocketAddress("127.0.0.1", 18899));
+            buffer.clear();
         }
+
+        //操作四：关闭 DatagramChannel 数据报通道
+        dChannel.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client().send();
     }
 }
---
+++ src/main/java/com/lifd/monkey/io/Server.java
@@ -0,0 +1,70 @@
+package com.lifd.monkey.io;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
+
+/**
+ * @author lifengdi
+ * @createTime 2025/2/8 11:26
+ */
+public class Server {
+    public void receive() throws IOException {
+        //获取 DatagramChannel 数据报通道
+        DatagramChannel datagramChannel = DatagramChannel.open();
+        //设置为非阻塞模式
+        datagramChannel.configureBlocking(false);
+        //绑定监听地址
+        datagramChannel.bind(
+                new InetSocketAddress("127.0.0.1", 18899));
+        System.out.println("UDP 服务器启动成功！");
+
+        //开启一个通道选择器
+        Selector selector = Selector.open();
+
+        //将通道注册到选择器
+        datagramChannel.register(selector, SelectionKey.OP_READ);
+
+        //通过选择器，查询 IO 事件
+        while (selector.select() > 0) {
+            Iterator<SelectionKey> iterator =
+                    selector.selectedKeys().iterator();
+            ByteBuffer buffer =
+                    ByteBuffer.allocate(1024);
+
+            //迭代 IO 事件
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+
+                //可读事件，有数据到来
+                if (selectionKey.isReadable()) {
+
+                    //读取 DatagramChannel 数据报通道的数据
+                    SocketAddress client =
+                            datagramChannel.receive(buffer);
+                    buffer.flip();
+                    System.out.println(new String(buffer.array(), 0, buffer.limit()));
+                    buffer.clear();
+                }
+            }
+            iterator.remove();
+        }
+
+        //关闭选择器和通道
+        selector.close();
+        datagramChannel.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Server().receive();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
